72c303b1421d14f33a1d927ff18c855866090f73
Shemar Jones <47797339+S-TJones@users.noreply.github.com>
2020-10-01 23:09:50 -0500

    Splash Screen v2
    Deleted the old file and added a new altogther. Still no luck, I'm tired. Gonna sleep it off.

--- app/src/main/java/com/example/androidapplicationdev/SplashScreen.kt
+++ app/src/main/java/com/example/androidapplicationdev/SplashScreen.kt
@@ -1,25 +1,20 @@
 package com.example.androidapplicationdev
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.support.wearable.activity.WearableActivity
-import androidx.core.content.ContextCompat.startActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
 
-class SplashScreen : WearableActivity() {
-
-    private val LOAD_TIME: Long = 3000
+class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
-        Handler().postDelayed({
-            startActivity(Intent (this,MainActivity::class.java))
-            finish()
-        }, LOAD_TIME)
-
-        // Enables Always-on
-        setAmbientEnabled()
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
 }
